c3dfb6323fc8deb93ccd276b0ec642dea6d2cd58
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-02-12 15:35:53 -0700

    variables for the password construction

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,69 @@
 // Assignment code here
-
+// Create variables containing the components of the password
+// Numbers
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+// Symbols
+const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+// Lowercase Characters
+const lowercaseCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+// Uppercase Characters
+const uppercaseCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+// The potential results = combination of numbers, symbols, and upper/lowercase characters
+let potentialResults = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +74,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
