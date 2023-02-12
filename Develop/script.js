f1bbbe157ded4aa6cba4ce66331deb4f9fc61278
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-02-12 16:18:22 -0700

    combine selections and create final result

--- Develop/script.js
+++ Develop/script.js
@@ -1,138 +1,162 @@
 // Assignment code here
-// Create variables containing the components of the password
-// Numbers
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-// Symbols
-const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-// Lowercase Characters
-const lowercaseCharacters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-// Uppercase Characters
-const uppercaseCharacters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-// The potential results = combination of numbers, symbols, and upper/lowercase characters
-let potentialResults = [];
-// Prompts for the user to select password options = upper, lower, numbers, symbols and length
-// Length of password
-numberOfCharacters = prompt("Select a number of characters between 8 and 128.");
-if (numberOfCharacters < 8 || numberOfCharacters > 128) {
-  // If less than 8 or greater than 128
-  return "Please choose a valid number of characters.";
-} else if (isNaN(numberOfCharacters)) {
-  // If not a number
-  numberOfCharacters = prompt("Please enter a valid number.");
-} else {
-  // Final prompt for length of password
-  alert("Your password will be " + numberOfCharacters + " characters long.");
-}
-// Use lowercase characters
-hasLowercase = confirm(
-  "Would you like your password to contain lowercase characters?"
-);
-if (hasLowercase) {
-  // Yes
-  let turnToLowercase = alert(
-    "Your password will contain lowercase characters."
+// The function to generate the password
+function generatePassword() {
+  // Create variables containing the components of the password
+  // Numbers
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  // Symbols
+  const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+  // Lowercase Characters
+  const lowercaseCharacters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  // Uppercase Characters
+  const uppercaseCharacters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  // The potential results = combination of numbers, symbols, and upper/lowercase characters
+  let potentialResults = [];
+  // Prompts for the user to select password options = upper, lower, numbers, symbols and length
+  // Length of password
+  numberOfCharacters = prompt(
+    "Select a number of characters between 8 and 128."
   );
-} else {
-  // No
-  alert("Your password will NOT contain lowercase characters.");
-}
-// Use uppercase characters
-hasLowercase = confirm(
-  "Would you like your password to contain lowercase characters?"
-);
-if (hasLowercase) {
-  // Yes
-  let turnToLowercase = alert(
-    "Your password will contain lowercase characters."
+  if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+    // If less than 8 or greater than 128
+    return "Please choose a valid number of characters.";
+  } else if (isNaN(numberOfCharacters)) {
+    // If not a number
+    numberOfCharacters = prompt("Please enter a valid number.");
+  } else {
+    // Final prompt for length of password
+    alert("Your password will be " + numberOfCharacters + " characters long.");
+  }
+  // Use lowercase characters
+  hasLowercase = confirm(
+    "Would you like your password to contain lowercase characters?"
   );
-} else {
-  // No
-  alert("Your password will NOT contain lowercase characters.");
-}
-// Use numbers
-hasNumbers = confirm("Would you like your password to contain numbers?");
-if (hasNumbers) {
-  // Yes
-  alert("Your password will contain numbers.");
-} else {
-  // No
-  alert("Your password will NOT contain numbers.");
-}
-// Use special characters aka symbols
-hasSpecial = confirm(
-  "Would you like your password to contain special characters?"
-);
-if (hasSpecial) {
-  alert("Your password will contain special characters.");
-} else {
-  alert("Your password will NOT contain special characters.");
-}
-// If user does not select any character options
-if (
-  hasLowercase === false &&
-  hasUppercase === false &&
-  hasNumbers === false &&
-  hasSpecial === false
-) {
-  return "Please select at least one character type.";
-}
+  if (hasLowercase) {
+    // Yes
+    let turnToLowercase = alert(
+      "Your password will contain lowercase characters."
+    );
+  } else {
+    // No
+    alert("Your password will NOT contain lowercase characters.");
+  }
+  // Use uppercase characters
+  hasUppercase = confirm(
+    "Would you like your password to conatin uppercase characters?"
+  );
+  if (hasUppercase) {
+    // Yes
+    alert("Your password will contain uppercase characters.");
+  } else {
+    // No
+    alert("Your password will NOT contain uppercase characters.");
+  }
+  // Use numbers
+  hasNumbers = confirm("Would you like your password to contain numbers?");
+  if (hasNumbers) {
+    // Yes
+    alert("Your password will contain numbers.");
+  } else {
+    // No
+    alert("Your password will NOT contain numbers.");
+  }
+  // Use special characters aka symbols
+  hasSpecial = confirm(
+    "Would you like your password to contain special characters?"
+  );
+  if (hasSpecial) {
+    alert("Your password will contain special characters.");
+  } else {
+    alert("Your password will NOT contain special characters.");
+  }
+  // If user does not select any character options
+  if (
+    hasLowercase === false &&
+    hasUppercase === false &&
+    hasNumbers === false &&
+    hasSpecial === false
+  ) {
+    return "Please select at least one character type.";
+  }
 
-
-
+  // Combine selected options (upper, lower, numbers, symbols) to create potentialResults
+  if (hasLowercase) {
+    potentialResults = potentialResults.concat(lowercaseCharacters);
+  }
+  if (hasUppercase) {
+    potentialResults = potentialResults.concat(uppercaseCharacters);
+  }
+  if (hasNumbers) {
+    potentialResults = potentialResults.concat(numbers);
+  }
+  if (hasSpecial) {
+    potentialResults = potentialResults.concat(symbols);
+  }
+  // pick random cards out of new pool for length of password
+  let finalResult = "";
+  for (let i = 0; i < numberOfCharacters; i++) {
+    let rng = [Math.floor(Math.random() * potentialResults.length)];
+    // or finalfinalResult += potentialResults[rng];
+    finalResult = finalResult + potentialResults[rng];
+  }
+  // Return the result as finalResult
+  return finalResult;
+}
+// Existing Source Code -->
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
